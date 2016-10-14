70a33db701648ef231f0927d5bf3c6c1a51aacb7
gaopengfei <gaopengfei@uniqueway.com>
2016-10-14 12:51:17 +0800

    增加通过用户搜索

--- blog/app/controllers/search_controller.rb
+++ blog/app/controllers/search_controller.rb
@@ -5,5 +5,9 @@ class SearchController < ApplicationController
     if params[:category]
       @articles = @articles.where(category: params[:category])
     end
+
+    if params[:user]
+      @articles = @articles.where user_id: params[:user]
+    end
   end
 end
