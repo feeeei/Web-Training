20a4468e69afd22868b4f862cbef2e1957dbb0bc
gaopengfei <gaopengfei@uniqueway.com>
2016-10-21 11:49:11 +0800

    增加生产环境下邮箱配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method =:smtp
+  config.action_mailer.default_url_options = {host: 'feeeei.com'}
+  config.action_mailer.delivery_method =:smtp
+  config.action_mailer.smtp_settings = {
+      :address=> "smtp.qq.com",
+      :port=> 25,
+      :domain=> "qq.com",
+      :authentication=> :login,
+      :user_name=> "540203430@qq.com",
+      :password=> "sbagyruebbmebbjg"
+  }
 end
