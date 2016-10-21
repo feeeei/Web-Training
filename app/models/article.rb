1aa2ee3dd54306bef8e81835683aab036673bb78
gaopengfei <gaopengfei@uniqueway.com>
2016-10-21 17:03:40 +0800

    调整文章列表默认为时间倒序排列

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   belongs_to :user
   validates :title, :text, :category, presence: true
   validates :user, presence: true
+
+  def Article.all
+    super.order(created_at: :desc)
+  end
 end
