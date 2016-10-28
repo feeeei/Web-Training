2c23d6ee5c9c3329fab1763e5e0b0e5294609704
gaopengfei <gaopengfei@uniqueway.com>
2016-10-28 16:15:45 +0800

    增加 shouda 测试

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  it { should have_many(:article) }
+
   describe User do
     it 'is valid with a email and password' do
       expect(build(:user)).to be_valid
